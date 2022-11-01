e791538b923d91eaa3a233e142266f01e638cf59
Curt Grogan <curt.grogan@fdmgroup.com>
2022-11-01 10:45:38 -0400

    MVP completed. All tests pass.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+/**
+ * install and use NPM multer package 
+ * reference: https://www.npmjs.com/package/multer
+ */
+const multer = require('multer');
+const upload = multer();
 
 var app = express();
 
@@ -11,8 +17,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post("/api/fileanalyse", upload.single('upfile'),(req, res) => {
+  // console.log("req.file: ", JSON.stringify(req.file));
+  const fileName = req.file.originalname;
+  const fileType = req.file.mimetype;
+  const fileSizeBytes = new Number(req.file.size);
+  res.json({name: fileName, type: fileType, size: fileSizeBytes});
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
